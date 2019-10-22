23c3b5b1dda22b9ed20d6cf2934ae5007bd54d20
Jaime Sastre <jaime.sastre@lapiscine.pro>
2019-10-22 10:50:38 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SugnupComponent } from './auth/sugnup/sugnup.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SugnupComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
